c4fae224ed0e9e77efbc7a11f1028fc0b7510f3f
RetrOrangePi <retrorangepi@outlook.com>
2017-11-06 01:23:26 +0100

    Tweaks to Background music

--- Background_Music/Disable_music.sh
+++ Background_Music/Disable_music.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pkill -f bgmusic.py
 touch /home/pi/RetrOrangePi/Background_Music/DisableMusic
-sudo sed --quiet -i -e 's/python/#Python/g' /etc/init.d/asplashscreen
-sudo sed --quiet -i -e 's/python/#Python/g' /opt/retropie/supplementary/splashscreen/asplashscreen.sh
+sudo sed -i -e 's/python/nohtyp/g' /etc/init.d/asplashscreen 2> /dev/null
+sudo sed -i -e 's/python/nohtyp/g' /opt/retropie/supplementary/splashscreen/asplashscreen.sh 2> /dev/null
 
--- Background_Music/Enable_music.sh
+++ Background_Music/Enable_music.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm /home/pi/RetrOrangePi/Background_Music/DisableMusic 2> /dev/null
-sudo sed --quiet -i -e 's/#Python/python/g' /etc/init.d/asplashscreen
-sudo sed --quiet -i -e 's/#Python/python/g' /opt/retropie/supplementary/splashscreen/asplashscreen.sh
+sudo sed -i -e 's/nohtyp/python/g' /etc/init.d/asplashscreen 2> /dev/null
+sudo sed -i -e 's/nohtyp/python/g' /opt/retropie/supplementary/splashscreen/asplashscreen.sh 2> /dev/null
 python /home/pi/RetrOrangePi/Background_Music/bgmusic.py &
+
 
