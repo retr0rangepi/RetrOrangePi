1fd57946a6aa7bc4a26b2293fc6a630f3245e165
retr0rangepi <retrorangepi@outlook.com>
2017-11-26 20:36:31 +0000

    Fixing background music on/off

--- Background_Music/Disable_music.sh
+++ Background_Music/Disable_music.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo pkill -f bgmusic.py
 touch /home/pi/RetrOrangePi/Background_Music/DisableMusic
-sudo sed -i -e 's/python/nohtyp/g' /etc/init.d/asplashscreen 2> /dev/null
-sudo sed -i -e 's/python/nohtyp/g' /opt/retropie/supplementary/splashscreen/asplashscreen.sh 2> /dev/null
+sudo sed -i -e 's/python/nohtyp/g' /opt/retropie/configs/all/autostart.sh 2> /dev/null
 
--- Background_Music/Enable_music.sh
+++ Background_Music/Enable_music.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm /home/pi/RetrOrangePi/Background_Music/DisableMusic 2> /dev/null
-sudo sed -i -e 's/nohtyp/python/g' /etc/init.d/asplashscreen 2> /dev/null
-sudo sed -i -e 's/nohtyp/python/g' /opt/retropie/supplementary/splashscreen/asplashscreen.sh 2> /dev/null
+sudo sed -i -e 's/nohtyp/python/g' /opt/retropie/configs/all/autostart.sh 2> /dev/null
 python /home/pi/RetrOrangePi/Background_Music/bgmusic.py &
 
 
