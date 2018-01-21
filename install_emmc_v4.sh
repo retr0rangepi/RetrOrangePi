ea2087b856b3ba74743379660f51a1874be67ce0
RetrOrangePi <retrorangepi@outlook.com>
2018-01-21 22:25:11 +0000

    Updating eMMC installer - fix to v4.1

---
+++ install_emmc_v4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pushd /home/pi/RetrOrangePi
+sudo ./emmc.installv4.SH
+sudo ./enable_emmc_sdcard.SH
+sudo mkdir /mnt/boot 
+sudo sed -i -e 's/mmc 0/mmc 1/g' /mnt/boot/boot-retro.cmd
+sudo mkimage -C none -A arm -T script -d /mnt/boot/boot-retro.cmd /mnt/boot/boot.scr >/dev/null 2>&1
+sudo umount /mnt/boot
+sudo rm -r /mnt/boot
+popd
+
