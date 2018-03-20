7458a10f6cb11af6351cd41348da3c25746de4f0
retr0rangepi <retrorangepi@outlook.com>
2018-03-20 13:46:17 +0000

    Tweaks to the eMMC installer

--- eMMC/Install_eMMC_v4.1.sh
+++ eMMC/Install_eMMC_v4.1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 pushd /home/pi/RetrOrangePi/eMMC
 sudo ./emmc.installv4.SH
-sudo ./enable_emmc_sdcard.SH
 sudo mkdir /mnt/boot 
 sudo mount /dev/mmcblk1p1 /mnt/boot
 sudo sed -i -e 's/mmc 0/mmc 1/g' /mnt/boot/boot-retro.cmd
 sudo sed -i -e 's/mmc 0/mmc 1/g' /mnt/boot/boot.kodi.cmd
 sudo mkimage -C none -A arm -T script -d /mnt/boot/boot-retro.cmd /mnt/boot/boot.scr >/dev/null 2>&1
+sudo ./enable_emmc_sdcard.SH
 sudo umount /mnt/boot
 sudo rm -r /mnt/boot
 popd
