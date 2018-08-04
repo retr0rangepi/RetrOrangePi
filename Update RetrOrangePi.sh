2ad6bc6b2df72262c6beac3f72fe9938d3f2af05
retrorangepi <retrorangepi@outlook.com>
2018-08-04 04:25:26 +0200

    first little fixes to slim image

--- Update RetrOrangePi.sh
+++ Update RetrOrangePi.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 mkdir ~/temp
-wget -O ~/temp/hotfix_v4.sh http://www.retrorangepi.org/hotfix_v4.sh
-chmod +x ~/temp/hotfix_v4.sh
-~/temp/hotfix_v4.sh
-pushd /home/pi/RetrOrangePi
-git pull
+wget -O ~/temp/hotfix_v4.2.sh http://www.retrorangepi.org/hotfix_v4.2.sh
+chmod +x ~/temp/hotfix_v4.2.sh
+~/temp/hotfix_v4.2.sh
 sudo killall login
 
--- eMMC/Install_eMMC_v3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pushd /home/pi/RetrOrangePi/eMMC
-sudo ./emmc.installv3.SH
-sudo ./enable_emmc_sdcard.SH
-popd
