64f69ad2dd6a82b0ceebfc4a2b482d8ac8cd87ce
retrorangepi <retrorangepi@outlook.com>
2018-08-19 16:44:44 +0200

    tweaks to bgmusic, thanks dee098

--- Background_Music/Disable_music.sh
+++ Background_Music/Disable_music.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo pkill -f bgmusic.py
 touch /home/pi/RetrOrangePi/Background_Music/DisableMusic
-sudo sed -i -e 's/python/nohtyp/g' /opt/retropie/configs/all/autostart.sh 2> /dev/null
+sudo sed -i -e 's/python/touch/g' /opt/retropie/configs/all/autostart.sh 2> /dev/null
 
--- Background_Music/Enable_music.sh
+++ Background_Music/Enable_music.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm /home/pi/RetrOrangePi/Background_Music/DisableMusic 2> /dev/null
-sudo sed -i -e 's/nohtyp/python/g' /opt/retropie/configs/all/autostart.sh 2> /dev/null
+sudo sed -i -e 's/touch/python/g' /opt/retropie/configs/all/autostart.sh 2> /dev/null
 python /home/pi/RetrOrangePi/Background_Music/bgmusic.py &
 
 
